06add229ba7519bc0c29c5ca75783a67550e6892
Karandra <32564546+Karandra@users.noreply.github.com>
2024-12-17 22:03:00 +0300

    Changed FSPath binary serialization order

--- kxf/FileSystem/FSPath.cpp
+++ kxf/FileSystem/FSPath.cpp
@@ -699,10 +699,10 @@ namespace kxf
 {
 	uint64_t BinarySerializer<FSPath>::Serialize(IOutputStream& stream, const FSPath& value) const
 	{
-		return Serialization::WriteObject(stream, value.m_Path) + Serialization::WriteObject(stream, value.m_Namespace);
+		return Serialization::WriteObject(stream, value.m_Namespace) + Serialization::WriteObject(stream, value.m_Path);
 	}
 	uint64_t BinarySerializer<FSPath>::Deserialize(IInputStream& stream, FSPath& value) const
 	{
-		return Serialization::ReadObject(stream, value.m_Path) + Serialization::ReadObject(stream, value.m_Namespace);
+		return Serialization::ReadObject(stream, value.m_Namespace) + Serialization::ReadObject(stream, value.m_Path);
 	}
 }
