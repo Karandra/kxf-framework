2364de2f9a40e4399bc7649d26adebeabc1985ce
Karandra <32564546+Karandra@users.noreply.github.com>
2024-12-17 16:01:33 +0300

    CallbackResult can now store a user-defined value in addition to the callback function state

---
+++ kxf/Core/CallbackFunction.cpp
@@ -0,0 +1,44 @@
+#include "KxfPCH.h"
+#include "CallbackFunction.h"
+
+namespace kxf
+{
+	size_t CallbackFunctionState::AsIndex(CallbackCommand command) const noexcept
+	{
+		auto index = ToInt(command);
+		if (index >= 0 && static_cast<size_t>(index) < m_Counters.size())
+		{
+			return static_cast<size_t>(index);
+		}
+		return std::numeric_limits<size_t>::max();
+	}
+	void CallbackFunctionState::UpdateWith(CallbackCommand command) noexcept
+	{
+		if (auto index = AsIndex(command); index < m_Counters.size())
+		{
+			m_Counters[index]++;
+			m_LastCommand = command;
+		}
+		else
+		{
+			m_LastCommand = CallbackCommand::Terminate;
+		}
+	}
+
+	size_t CallbackFunctionState::GetCount() const noexcept
+	{
+		return GetCount(CallbackCommand::Continue) + GetCount(CallbackCommand::Discard);
+	}
+	size_t CallbackFunctionState::GetCount(CallbackCommand command) const noexcept
+	{
+		if (auto index = AsIndex(command); index < m_Counters.size())
+		{
+			return m_Counters[index];
+		}
+		return 0;
+	}
+	size_t CallbackFunctionState::GetTotalCount() const noexcept
+	{
+		return std::accumulate(m_Counters.begin(), m_Counters.end(), 0_uz);
+	}
+}
--- kxf/Core/CallbackFunction.h
+++ kxf/Core/CallbackFunction.h
@@ -15,51 +15,146 @@ namespace kxf
 
 namespace kxf
 {
-	class KX_API CallbackResult final
+	class KX_API CallbackFunctionState final
 	{
+		template<class... Args_>
+		friend class CallbackFunction;
+
 		private:
 			std::array<size_t, 3> m_Counters;
 			CallbackCommand m_LastCommand = CallbackCommand::None;
 
+		private:
+			size_t AsIndex(CallbackCommand command) const noexcept;
+			void UpdateWith(CallbackCommand command) noexcept;
+
 		public:
-			CallbackResult() noexcept
+			CallbackFunctionState() noexcept
 			{
 				m_Counters.fill(0);
 			}
-
-		public:
-			void UpdateWith(CallbackCommand command)
+			CallbackFunctionState(const CallbackFunctionState&) noexcept = default;
+			CallbackFunctionState(CallbackFunctionState&& other) noexcept
 			{
-				if (auto index = ToInt(command); index >= 0 && static_cast<size_t>(index) < m_Counters.size())
-				{
-					m_Counters[static_cast<size_t>(index)]++;
-					m_LastCommand = command;
-				}
-				else
-				{
-					m_LastCommand = CallbackCommand::Terminate;
-				}
+				*this = std::move(other);
 			}
 
+		public:
+			size_t GetCount() const noexcept;
+			size_t GetCount(CallbackCommand command) const noexcept;
+			size_t GetTotalCount() const noexcept;
 			CallbackCommand GetLastCommand() const noexcept
 			{
 				return m_LastCommand;
 			}
-			size_t GetInvokeCount(CallbackCommand command) const noexcept
+
+		public:
+			CallbackFunctionState& operator=(const CallbackFunctionState&) noexcept = default;
+			CallbackFunctionState& operator=(CallbackFunctionState&& other) noexcept
 			{
-				if (auto index = ToInt(command); index >= 0 && static_cast<size_t>(index) < m_Counters.size())
+				if (this != &other)
 				{
-					return m_Counters[static_cast<size_t>(index)];
+					m_Counters = other.m_Counters;
+					other.m_Counters.fill(0);
+
+					m_LastCommand = std::exchange(other.m_LastCommand, CallbackCommand::None);
 				}
-				return 0;
+				return *this;
 			}
-			size_t GetTotalInvokeCount() const noexcept
+	};
+
+	template<class T>
+	class CallbackResult final
+	{
+		public:
+			using TResult = typename T;
+
+		private:
+			CallbackFunctionState m_State;
+			std::optional<T> m_Result;
+
+		public:
+			CallbackResult() noexcept = default;
+			CallbackResult(CallbackFunctionState state, T result)
+				:m_State(std::move(state)), m_Result(std::move(result))
 			{
-				return std::accumulate(m_Counters.begin(), m_Counters.end(), static_cast<size_t>(0));
 			}
-			size_t GetSuccessInvokeCount() const noexcept
+
+		public:
+			bool IsNull() const noexcept
 			{
-				return GetInvokeCount(CallbackCommand::Continue);
+				return m_State.GetLastCommand() != CallbackCommand::None && m_Result.has_value();
+			}
+			T Take() noexcept(std::is_nothrow_move_constructible_v<T>)
+			{
+				return std::exchange(m_Result, std::nullopt).value();
+			}
+
+			CallbackFunctionState GetState() const noexcept
+			{
+				return m_State;
+			}
+			size_t GetCount() const noexcept
+			{
+				return m_State.GetCount();
+			}
+
+		public:
+			explicit operator bool() const noexcept
+			{
+				return !IsNull();
+			}
+			bool operator!() const noexcept
+			{
+				return IsNull();
+			}
+
+			T& operator*() noexcept
+			{
+				return *m_Result;
+			}
+			const T& operator*() const noexcept
+			{
+				return *m_Result;
+			}
+	};
+
+	template<>
+	class CallbackResult<void> final
+	{
+		private:
+			CallbackFunctionState m_State;
+
+		public:
+			CallbackResult() noexcept = default;
+			CallbackResult(CallbackFunctionState state) noexcept
+				:m_State(std::move(state))
+			{
+			}
+
+		public:
+			bool IsNull() const noexcept
+			{
+				return m_State.GetLastCommand() == CallbackCommand::None;
+			}
+
+			CallbackFunctionState GetState() const noexcept
+			{
+				return m_State;
+			}
+			size_t GetCount() const noexcept
+			{
+				return m_State.GetCount();
+			}
+
+		public:
+			explicit operator bool() const noexcept
+			{
+				return !IsNull();
+			}
+			bool operator!() const noexcept
+			{
+				return IsNull();
 			}
 	};
 }
@@ -148,7 +243,19 @@ namespace kxf
 	class CallbackFunction final: public BasicCallbackFunction<CallbackCommand, Args_...>
 	{
 		private:
-			CallbackResult m_Result;
+			CallbackFunctionState m_State;
+
+		private:
+			void OnFinalize() noexcept
+			{
+				// It the callback wasn't called even once (because there were 0 elements
+				// to iterate over, for example), set the last command to CallbackCommand::Discard
+				// to indicate that the operation was successfully completed.
+				if (m_State.m_LastCommand == CallbackCommand::None)
+				{
+					m_State.m_LastCommand = CallbackCommand::Discard;
+				}
+			}
 
 		public:
 			CallbackFunction() noexcept = default;
@@ -172,39 +279,48 @@ namespace kxf
 			}
 
 		public:
-			void Reset() noexcept
-			{
-				m_Result = {};
-			}
-			bool ShouldTerminate() const noexcept
-			{
-				return m_Result.GetLastCommand() == CallbackCommand::Terminate;
-			}
-
 			CallbackFunction& Invoke(Args_... arg)
 			{
 				if (this->m_Callable)
 				{
 					CallbackCommand command = std::invoke(this->m_Callable, std::forward<Args_>(arg)...);
-					m_Result.UpdateWith(command);
+					m_State.UpdateWith(command);
 				}
 				else
 				{
-					m_Result.UpdateWith(CallbackCommand::Discard);
+					m_State.UpdateWith(CallbackCommand::Discard);
 				}
 				return *this;
 			}
+			bool ShouldTerminate() const noexcept
+			{
+				return m_State.GetLastCommand() == CallbackCommand::Terminate;
+			}
+			void ResetState() noexcept
+			{
+				m_State = {};
+			}
+
+			CallbackFunctionState GetState() const noexcept
+			{
+				return m_State;
+			}
 			CallbackCommand GetLastCommand() const noexcept
 			{
-				return m_Result.GetLastCommand();
+				return m_State.GetLastCommand();
 			}
-			CallbackResult GetResult() const
+
+			CallbackResult<void> Finalize() noexcept
 			{
-				return m_Result;
+				OnFinalize();
+				return std::move(m_State);
 			}
-			size_t GetCount() const noexcept
+
+			template<class T>
+			CallbackResult<T> Finalize(T result)
 			{
-				return m_Result.GetSuccessInvokeCount();
+				OnFinalize();
+				return {std::move(m_State), std::move(result)};
 			}
 	};
 };
